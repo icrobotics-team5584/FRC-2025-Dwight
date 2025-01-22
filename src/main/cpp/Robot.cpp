9bd7ed7cf4aa578ae99b43ba00e0100f2c33dd77
SaumYazdi <kho0042@mhs.vic.edu.au>
2025-01-22 13:29:20 +1100

    .

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -5,7 +5,6 @@
 #include "Robot.h"
 #include "subsystems/SubElevator.h"
 #include "commands/CoralCommands.h"
-
 #include "frc/DataLogManager.h"
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <frc2/command/CommandScheduler.h>
