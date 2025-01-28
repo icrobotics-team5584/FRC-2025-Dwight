6aeecb204cf1af86998a376a4214f87a7da6f4bc
Liam Stow <liam.stow@outlook.com>
2025-01-28 21:14:42 +1100

    adjust driving angle while dumbVisionAligning

--- src/main/cpp/commands/VisionCommand.cpp
+++ src/main/cpp/commands/VisionCommand.cpp
@@ -43,11 +43,11 @@ frc2::CommandPtr ForceAlignWithTarget(int side, frc2::CommandXboxController& con
   // reef so you stay aligned rotationally and at the right distance.
   return SubDrivebase::GetInstance().Drive(
       [side, &controller] {
-        units::degree_t goalAngle = side == 1 ? 30_deg : -20_deg;
-        units::degree_t driveAngle = -20_deg;
+        units::degree_t goalAngle = side == 1 ? 15.60_deg : -20_deg; // 16.45 for left reef face, 15.60_deg for front reef face (all left side so far)
         units::degree_t tagAngle = SubVision::GetInstance().GetReefTagAngle();
         units::degree_t error = tagAngle - goalAngle;
         units::meters_per_second_t overallVelocity = 0.05_mps * error.value();
+        units::degree_t driveAngle = units::math::copysign(3_deg, error);
         // Calc x and y from known angle and hypotenuse length
         units::meters_per_second_t xVel = overallVelocity * units::math::cos(driveAngle);
         units::meters_per_second_t yVel = overallVelocity * units::math::sin(driveAngle);
