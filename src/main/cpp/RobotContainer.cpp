042e4b11fb008629c042718b79dd2c4900eda933
furmena <diaschithira21@gmail.com>
2025-02-16 10:57:46 +1100

    added moveforewardrotate autons

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -69,12 +69,16 @@ RobotContainer::RobotContainer() {
   _autoChooser.AddOption("L-Shape-Spinning", "L-Shape-Spinning");
   _autoChooser.AddOption("L-Shape-Spinning-Slow", "L-Shape-Spinning-Slow");
   _autoChooser.AddOption("move", "move");
+
   _autoChooser.AddOption("WheelCharecterisation-4m-0.1ms", "MoveForward-4M-0.1ms");
   _autoChooser.AddOption("WheelCharecterisation-4m-0.5ms", "MoveForward-4M-0.5ms");
   _autoChooser.AddOption("WheelCharecterisation-4m-1.0ms", "MoveForward-4M-1.0ms");
   _autoChooser.AddOption("WheelCharecterisation-4m-1.5ms", "MoveForward-4M-1.5ms");
   _autoChooser.AddOption("WheelCharecterisation-4m-2.0ms", "MoveForward-4M-2.0ms");
+  _autoChooser.AddOption("WheelCharecterisation-4m-0.1ms-360rot", "MoveForwardRotate-4M-0.1ms");
+  _autoChooser.AddOption("WheelCharecterisation-4m-1.0ms-360rot", "MoveForwardRotate-4M-1.0ms");
   _autoChooser.AddOption("WheelCharecterisation-7ishm-0.1ms", "MoveForward-7ishM-0.1ms");
+
   _autoChooser.AddOption("SpinInSpot-180", "SpinInSpot-180");
   _autoChooser.AddOption("SpinInSpot-180-Slow", "SpinInSpot-180-Slow");
   _autoChooser.AddOption("SpinInSpot-360", "SpinInSpot-360");
