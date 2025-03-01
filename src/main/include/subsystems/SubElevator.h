420b189e04d2094eb3cf9fddf5b1db86a4b07e4a
Liam Stow <liam.stow@outlook.com>
2025-03-01 18:21:26 +1100

    slightly higher elevator height for climb

--- src/main/include/subsystems/SubElevator.h
+++ src/main/include/subsystems/SubElevator.h
@@ -72,7 +72,7 @@ class SubElevator : public frc2::SubsystemBase {
   static constexpr units::meter_t _ALGAE_LOW_HEIGHT = 0.78_m;
   static constexpr units::meter_t _ALGAE_HIGH_HEIGHT = 1.242_m;
   static constexpr units::meter_t _SOURCE_HEIGHT = 0.01_m;
-  static constexpr units::meter_t _CLIMB_HEIGHT = 0.722_m;
+  static constexpr units::meter_t _CLIMB_HEIGHT = 0.782_m;
   static constexpr units::meter_t _CLEAR_LATCH_HEIGHT = 0.863_m;
 
  private:
