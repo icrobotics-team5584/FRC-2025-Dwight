71a0e0576c4b2e01f5cc3ea264ec982e792461b1
furmena <diaschithira21@gmail.com>
2025-03-06 16:41:32 +1100

    added auton into autonchooser

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -55,6 +55,7 @@ RobotContainer::RobotContainer() {
 
   // main autons
   _autoChooser.AddOption("Default-Left", "Default-Score3L4-Vision");
+  _autoChooser.AddOption("Default-Right", "Right-Score3L4-Vision");
   _autoChooser.AddOption("DefaultMiddle-ScoreLeft", "Default-Score1L4-G-Vision");
   _autoChooser.AddOption("DefaultMiddle-ScoreRight", "Default-Score1L4-H-Vision");
 
