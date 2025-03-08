ee02cc37d8843528ed8d805a25b5e661320c04e2
ICWorkshop <icroboticsfll@gmail.com>
2025-03-08 11:42:52 +1100

    added hopeAndShoot to last auto coral

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -28,6 +28,7 @@ RobotContainer::RobotContainer() {
   // registar named commands
   pathplanner::NamedCommands::registerCommand("ScoreLeft-WithVision", cmd::ScoreWithVision(SubVision::Side::Left));
   pathplanner::NamedCommands::registerCommand("ScoreRight-WithVision", cmd::ScoreWithVision(SubVision::Side::Right));
+  pathplanner::NamedCommands::registerCommand("ScoreLeft-WithPrescription", cmd::ScoreWithPrescription(SubVision::Side::Left));
   pathplanner::NamedCommands::registerCommand("ScoreLeft", cmd::Score(1));
   pathplanner::NamedCommands::registerCommand("ScoreRight", cmd::Score(2));
   pathplanner::NamedCommands::registerCommand("SetElevatorL4", SubElevator::GetInstance().CmdSetL4());
--- src/main/cpp/commands/AutonCommands.cpp
+++ src/main/cpp/commands/AutonCommands.cpp
@@ -73,4 +73,27 @@ namespace cmd {
                 .Until([]{ return SubElevator::GetInstance().IsAtTarget(); })
             );
     }
+
+    frc2::CommandPtr ScoreWithPrescription(SubVision::Side side) {
+        static frc::Timer timer;
+
+        return SubElevator::GetInstance().CmdSetL4()//.OnlyIf([] { return !SubElevator::GetInstance().IsAtTarget(); })
+            .AndThen(frc2::cmd::WaitUntil([]{ return SubElevator::GetInstance().IsAtTarget(); }))
+            .AndThen([]{timer.Restart();})
+            .AndThen(cmd::HopeAndShoot(side).Until([]{ 
+                bool has_coral = SubEndEffector::GetInstance().CheckLineBreakHigher();
+                if ( has_coral == false) {
+                    Logger::Log("EndEffector/ScoreWithVision/has coral", false);
+                    timer.Start();
+                } else {
+                    Logger::Log("EndEffector/ScoreWithVision/has coral", true);
+                    timer.Restart();
+                }
+                Logger::Log("EndEffector/ScoreWithVision/time elapsed without coral", timer.Get());
+                return timer.HasElapsed(0.5_s); }))
+            .AndThen(SubElevator::GetInstance().CmdSetSource())
+            .AndThen(SubEndEffector::GetInstance().RemoveAlgae()
+                .Until([]{ return SubElevator::GetInstance().IsAtTarget(); })
+            );
+    }
 }
