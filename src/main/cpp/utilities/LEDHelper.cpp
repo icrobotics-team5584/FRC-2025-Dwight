f5b4e5cdae42030a6ff57b8840209660317a9c45
Jared PC <jared.jj.jin@gmail.com>
2025-04-26 22:05:55 +1000

    added some LEDPattern functions, such as SetFire, SetScrollingRainbow etc

--- src/main/cpp/utilities/LEDHelper.cpp
+++ src/main/cpp/utilities/LEDHelper.cpp
@@ -4,20 +4,47 @@
 
 
 void LEDHelper::SetSolidColour(frc::Color color) {
-    for (int i = 0; i < kLength; i++) {
-        m_ledBuffer[i].SetRGB(color.red, color.green, color.blue);
-    }
+    frc::LEDPattern ledpattern = frc::LEDPattern::Solid(color);
+    ledpattern.ApplyTo(m_ledBuffer);
     m_led.SetData(m_ledBuffer);
 }
 
-void LEDHelper::SetRainbow() {
-    static int rainbowFirstPixelHue = 0;
-    for (int i = 0; i < kLength; i++) {
-        const int hue = (rainbowFirstPixelHue + (i * 180 / kLength)) % 180;
-        m_ledBuffer[i].SetHSV(hue, 255, 128);
-    }
-    rainbowFirstPixelHue += 3;
-    rainbowFirstPixelHue %= 180;
+void LEDHelper::SetScrollingRainbow() {
+    frc::LEDPattern m_rainbow = frc::LEDPattern::Rainbow(255, 128);
+    frc::LEDPattern m_scrollingRainbow = m_rainbow.ScrollAtAbsoluteSpeed(units::velocity::meters_per_second_t{1}, kLedSpacing); //might need to continuously apply in robot periodic??
+    m_led.SetData(m_ledBuffer);
+}
+
+void LEDHelper::SetContinuousGradient(frc::Color Color1, frc::Color Color2) {
+    std::array<frc::Color, 2> colors{Color1, Color2};
+    frc::LEDPattern gradient = frc::LEDPattern::Gradient(frc::LEDPattern::GradientType::kContinuous, colors);
+
+    // Apply the LED pattern to the data buffer
+    gradient.ApplyTo(m_ledBuffer);
+    m_led.SetData(m_ledBuffer);
+}
+
+void LEDHelper::SetFire() {
+    double randomValue = 0.5 + static_cast<double>(rand()) / (static_cast<double>(RAND_MAX / 0.5));
+
+    std::array<frc::Color, 2> colors{frc::Color::kRed, frc::Color::kWhite};
+    frc::LEDPattern base = frc::LEDPattern::Gradient(frc::LEDPattern::GradientType::kContinuous, colors);
+    frc::LEDPattern mask = frc::LEDPattern::ProgressMaskLayer([&]() { return randomValue; });
+    
+    frc::LEDPattern heightDisplay = base.Mask(mask);
+
+    // Apply the LED pattern to the data buffer
+    heightDisplay.ApplyTo(m_ledBuffer);
+    m_led.SetData(m_ledBuffer);
+}
+
+void LEDHelper::SetBreatheColour(frc::Color color) {
+
+    frc::LEDPattern base = frc::LEDPattern::Solid(color);
+    frc::LEDPattern pattern = base.Breathe(2_s);
+
+    // Apply the LED pattern to the data buffer
+    pattern.ApplyTo(m_ledBuffer);
     m_led.SetData(m_ledBuffer);
 }
 
--- src/main/include/utilities/LEDHelper.h
+++ src/main/include/utilities/LEDHelper.h
@@ -1,17 +1,23 @@
 #pragma once
 
 #include <frc/AddressableLED.h>
+#include <frc/LEDPattern.h>
+#include <units/length.h>
 
 class LEDHelper {
 public:
   void Start();
 
   void SetSolidColour(frc::Color color);
-  void SetRainbow();
+  void SetScrollingRainbow();
+  void SetContinuousGradient(frc::Color Color1, frc::Color Color2);
+  void SetBreatheColour(frc::Color color);
+  void SetFire();
   void FlashColour(frc::Color color);
 
 private:
   static constexpr int kLength = 60;
+  units::meter_t kLedSpacing = (1/50)*1_m; // CHANGE LATER this is for 50 leds every meter
 
 
   // PWM port 9 as a dummy value
