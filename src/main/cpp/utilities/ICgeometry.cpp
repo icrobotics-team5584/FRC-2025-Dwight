bb7bd43de7564fc9a3bb9917b91194e6e6b31080
furmena <diaschithira21@gmail.com>
2025-05-14 20:59:58 +1000

    updated icgeometry to add x,y,xy flip

--- src/main/cpp/utilities/ICgeometry.cpp
+++ src/main/cpp/utilities/ICgeometry.cpp
@@ -1,7 +1,25 @@
+#include "utilities/ICgeometry.h"
+#include <units/length.h>
 #include <frc/geometry/Pose2d.h>
 
 namespace ICgeometry { 
-    frc::Pose2d ClosestPoseVector(frc::Pose2d startPose, std::vector<frc::Pose2d> poseVector) {
+    frc::Pose2d xFilpPose(frc::Pose2d pose) {
+        units::meter_t xdiff = units::math::abs(FIELD_LENGTH/2 - pose.Y());
+        units::meter_t x = (pose.X() < FIELD_LENGTH/2) ? FIELD_LENGTH/2 + xdiff : FIELD_LENGTH/2 - xdiff;
+        return frc::Pose2d(x, pose.Y(), pose.Rotation());
+    }
+
+    frc::Pose2d yFilpPose(frc::Pose2d pose) {
+        units::meter_t ydiff = units::math::abs(FIELD_WIDTH/2 - pose.Y());
+        units::meter_t y = (pose.Y() < FIELD_WIDTH/2) ? FIELD_WIDTH/2 + ydiff : FIELD_WIDTH/2 - ydiff;
+        return frc::Pose2d(pose.X(), y, pose.Rotation());
+    }
+
+    frc::Pose2d xyFilpPose(frc::Pose2d pose) {
+        return yFilpPose(xFilpPose(pose));
+    }
+
+    frc::Pose2d closestPoseVector(frc::Pose2d startPose, std::vector<frc::Pose2d> poseVector) {
         if (poseVector.empty()) { return startPose; }
         double shortestDistance = 1024.0;
         double curDistance;
--- src/main/include/utilities/ICgeometry.h
+++ src/main/include/utilities/ICgeometry.h
@@ -1,5 +1,12 @@
 #include <frc/geometry/Pose2d.h>
 
-namespace ICgometry { 
-    frc::Pose2d ClosestPoseVector(frc::Pose2d startPose, std::vector<frc::Pose2d> poseVector);
+namespace ICgeometry { 
+    const units::meter_t FIELD_WIDTH = 8.05_m;
+    const units::meter_t FIELD_LENGTH = 17.55_m;
+
+    frc::Pose2d closestPoseVector(frc::Pose2d startPose, std::vector<frc::Pose2d> poseVector);
+    
+    frc::Pose2d xPoseFlip(frc::Pose2d pose);
+    frc::Pose2d yPoseFlip(frc::Pose2d pose);
+    frc::Pose2d xyPoseFlip(frc::Pose2d pose);
 }
