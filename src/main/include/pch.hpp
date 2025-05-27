55b082664cf4586e025d00ebabea10dec9a6c1a1
Liam Stow <liam.stow@outlook.com>
2025-05-27 21:44:39 +1000

    added compilePch task to build.gradle

---
+++ src/main/include/pch.hpp
@@ -0,0 +1,11 @@
+#ifndef PCH_HPP
+#define PCH_HPP
+
+
+#include <rev/SparkFlex.h>
+#include <rev/SparkMax.h>
+#include "utilities/BotVars.h"
+// #include "ctre/phoenix6/hardware/ParentDevice.hpp"
+
+
+#endif // PCH_HPP
