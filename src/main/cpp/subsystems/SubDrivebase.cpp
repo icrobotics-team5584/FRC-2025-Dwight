26fb8ccb4967aaf419a1b2e0af6f761b36bf31c4
Jared PC <jared.jj.jin@gmail.com>
2025-10-02 21:29:26 +1000

    removed "reef" in comment

--- src/main/cpp/subsystems/SubDrivebase.cpp
+++ src/main/cpp/subsystems/SubDrivebase.cpp
@@ -95,7 +95,7 @@ void SubDrivebase::Periodic() {
   SubDrivebase::GetInstance().GetSlippingModule();
   
   frc::SmartDashboard::PutNumber("Drivebase/AccelerationX", _gyro.GetAccelerationX().GetValueAsDouble());
-  frc::SmartDashboard::PutNumber("Drivebase/AccelerationY", _gyro.GetAccelerationY().GetValueAsDouble()); // smashed into reef with -1.2g and -0.4g 
+  frc::SmartDashboard::PutNumber("Drivebase/AccelerationY", _gyro.GetAccelerationY().GetValueAsDouble()); // smashed into wall with -1.2g and -0.4g of deceleration
   frc::SmartDashboard::PutNumber("Drivebase/AccelerationZ", _gyro.GetAccelerationZ().GetValueAsDouble()); // normal driving goes up to around the same -1.0g
   frc::SmartDashboard::PutNumber("Drivebase/IsCollision", SubDrivebase::GetInstance().IsCollision());
 
